395487bb3507eef0d345256872aebb8e53fac69a
Artsiom Vaskevich <48159601+FLAPJVCK@users.noreply.github.com>
2022-11-28 17:51:02 +0100

    refactored first task (#2)

---
+++ src/components/layouts/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Logo from '../logo/Logo';
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <footer className='footer'>
+            <Logo/>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/components/layouts/header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Logo from '../logo/Logo';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <Logo/>
+        </header>
+    );
+};
+
+export default Header;
