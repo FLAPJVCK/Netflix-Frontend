98895e036134f14bf7ef7e5802986ec3b182d769
Artsiom Vaskevich <avaskevich@exadel.com>
2022-12-22 03:37:51 +0100

    completed task-06

--- src/components/modal/ModalWindow.jsx
+++ src/components/modal/ModalWindow.jsx
@@ -1,27 +1,23 @@
 import React from 'react';
 import './ModalWindow.css'
 import crossImage from '../../images/x.svg'
-import axios from "axios";
-import {MOVIE_URL} from "../../consts";
 
-const ModalWindow = ({active, setActive, children, secondaryText, submitText, movie}) => {
-
-    function submit(){
-        axios.post(MOVIE_URL, movie)
-    }
+const ModalWindow = ({active, setActive, children, secondaryText, submitText, formik}) => {
 
     return (
         <div className={active ? 'modal-window active' : 'modal-window'} onClick={() => setActive(false)}>
             <form className={active ? 'modal-window_content active' : 'modal-window_content'}
-                 onClick={e => e.stopPropagation()} onSubmit={() => submit()}>
+                  onClick={e => e.stopPropagation()} onSubmit={e => formik.handleSubmit(e)}>
                 <button className='modal-window__exit-button' onClick={() => setActive(false)}>
                     <img className='modal-window__exit-image' src={crossImage} alt='Cross'/>
                 </button>
                 {children}
                 {(secondaryText || submitText) &&
                     <div className="modal-window__buttons">
-                        {secondaryText && <button className='modal-window__buttons_secondary-text' type='reset'>{secondaryText}</button>}
-                        {submitText && <button className='modal-window__buttons_submit-text' type='submit'>{submitText}</button>}
+                        {secondaryText && <button className='modal-window__buttons_secondary-text' type='reset'
+                                                  onClick={e => formik.resetForm()}>{secondaryText}</button>}
+                        {submitText && <button className='modal-window__buttons_submit-text' type='submit'
+                                               disabled={!formik.isValid}>{submitText}</button>}
                     </div>
                 }
             </form>
